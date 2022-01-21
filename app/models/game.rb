b6680087cb3c62e55df949855b8c05a36154364e
DylanToporek <dylantoporek@gmail.com>
2022-01-21 16:35:30 -0500

    validations added

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :user
+  validates :player_avatar, presence: true
+  validates :cpu1_avatar, presence: true
+  validates :cpu2_avatar, presence: true
+  validates :cpu3_avatar, presence: true
 end
