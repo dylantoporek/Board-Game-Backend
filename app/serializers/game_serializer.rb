c8b2b695bf788fccc032868442b35ce30d9d3832
DylanToporek <dylantoporek@gmail.com>
2022-01-21 16:31:59 -0500

    serializers updated

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,4 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :player_avatar, :player_position, :cpu1_avatar, :cpu1_position, :cpu2_avatar, :cpu2_position, :cpu3_avatar, :cpu3_position
+  has_one :user
 end
