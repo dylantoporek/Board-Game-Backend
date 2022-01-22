7af235c5c1851a4d29db8ae86b667edd193bb181
DylanToporek <dylantoporek@gmail.com>
2022-01-22 12:01:58 -0500

    controllers updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,16 @@
 class GamesController < ApplicationController
+    def index
+        render json: Game.all
+    end
+
+    def create
+        game = @current_user.games.create!(game_params)
+        render json: game, status: :created
+    end
+
+    private
+
+    def game_params
+        params.permit(:player_avatar, :player_position, :cpu1_avatar, :cpu1_position, :cpu2_avatar, :cpu2_position, :cpu3_avatar, :cpu3_position)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :games
-  resources :users
+  resources :games, only: [:index, :create]
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
