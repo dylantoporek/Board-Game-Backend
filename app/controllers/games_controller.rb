e2a51da436cfde2d5db736d00ed847aff631d201
DylanToporek <dylantoporek@gmail.com>
2022-01-25 13:45:20 -0500

    update and delete added to /games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,16 +1,40 @@
 class GamesController < ApplicationController
+
+    # get /games
     def index
         render json: Game.all
     end
 
+    # post /games
     def create
         game = @current_user.games.create!(game_params)
         render json: game, status: :created
     end
 
+    # get /games/id
     def show
         game = @current_user.games.find_by(id: params[:id])
         render json: game, status: :created
+    end
+
+    # delete /games/id
+    def destroy
+        game = @current_user.games.find_by(id: params[:id])
+        game.delete
+        head :no_content
+    end
+
+    # patch /games/id
+
+    def update
+        game = @current_user.games.find_by(id: params[:id])
+        game.update(
+            player_position: params[:player_position],
+            cpu1_position: params[:cpu1_position],
+            cpu2_position: params[:cpu2_position],
+            cpu3_position: params[:cpu3_position]
+        )
+        render json: game, status: :ok
     end
 
     private
