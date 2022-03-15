ce16f5b83d94714ceeb948e0dd3de83a5d932d30
DylanToporek <dylantoporek@gmail.com>
2022-03-15 16:48:58 -0400

    cores updates

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://hungry-nightingale-1b562f.netlify.app/"
-    
+    origins "https://hungry-nightingale-1b562f.netlify.app"
+
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
