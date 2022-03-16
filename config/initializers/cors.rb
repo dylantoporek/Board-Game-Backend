b690268f951e5eaa0d302723b09ffc0d2cda80f5
DylanToporek <dylantoporek@gmail.com>
2022-03-16 17:05:41 -0400

    origin update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins "https://hungry-nightingale-1b562f.netlify.app"
-    origins "http://localhost:3000"
+    origins "https://hungry-nightingale-1b562f.netlify.app"
+    # origins "http://localhost:3000"
 
     resource "*",
       headers: :any,
